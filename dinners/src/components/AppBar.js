ee7fbde7c99a92f3e07843fce94e9ed73e152fd5
Matt <mattkravetz@gmail.com>
2018-08-19 17:29:45 -0400

    Added routing

--- dinners/src/components/AppBar.js
+++ dinners/src/components/AppBar.js
@@ -2,16 +2,20 @@ import React from "react";
 
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
 import { Button } from "@material-ui/core";
+
+import { Link } from 'react-router-dom'
+
+const DinnersLink = props => <Link to="/dinners" {...props} />
+const ShoppingListLink = props => <Link to="/shoppinglist" {...props} />
 
 function ButtonAppBar(props) {
   return (
     <div>
       <AppBar position="static">
         <Toolbar>
-          <Button color="inherit">Dinners</Button>
-          <Button color="inherit">Shopping List</Button>
+          <Button component={DinnersLink} color="inherit">Dinners</Button>
+          <Button component={ShoppingListLink} color="inherit">Shopping List</Button>
         </Toolbar>
       </AppBar>
     </div>

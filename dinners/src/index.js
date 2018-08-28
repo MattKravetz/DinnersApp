ee843983e30d0019408777c140f9ef31323da6d9
Matt <mattkravetz@gmail.com>
2018-08-27 23:42:23 -0400

    Added localstorage

--- dinners/src/index.js
+++ dinners/src/index.js
@@ -6,16 +6,18 @@ import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter } from "react-router-dom";
 
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import persistState from "redux-localstorage";
+import { compose, createStore } from "redux";
 import dinnersApp from "./reducers/dinners_app";
 // import initialState from './reducers/initial_state'
-import {testState as initialState} from './test/initial_state'
+import { testState as initialState } from "./test/initial_state";
 
-const store = createStore(
-  dinnersApp,
-  initialState,
+const enhancer = compose(
+  persistState(),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+const store = createStore(dinnersApp, initialState, enhancer);
 
 ReactDOM.render(
   <Provider store={store}>

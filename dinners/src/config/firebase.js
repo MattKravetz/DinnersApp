caec1a370f9c45be82ff4bef51e406d41cb3e9a2
Matt <mattkravetz@gmail.com>
2018-09-01 21:07:48 -0400

    Hooked initial state up to firebase

--- dinners/src/config/firebase.js
+++ dinners/src/config/firebase.js
@@ -2,7 +2,17 @@ import * as firebase from "firebase";
 
 import { FirebaseConfig } from "./keys";
 
-firebase.initializeApp(FirebaseConfig)
+firebase.initializeApp(FirebaseConfig);
 
 export const database = firebase.database();
 
+// todo: use thunk, make this an action creator, 
+// dispatch in index.js before initializing the provier.  
+// Remove the inital state param from store instantiation
+export const getInitialState = () => {    
+    const state = database
+    .ref("/")
+    .once("value")
+    .then(snapShot => snapShot.val());
+  return state;
+};
--- dinners/src/index.js
+++ dinners/src/index.js
@@ -7,18 +7,18 @@ import { BrowserRouter } from "react-router-dom";
 
 import { Provider } from "react-redux";
 import persistState from "redux-localstorage";
-import { compose, createStore } from "redux";
+import { compose, createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import dinnersApp from "./reducers/index";
-// import initialState from './reducers/initial_state'
-import { testState as initialState } from "./test/initial_state";
+import { getInitialState } from "./actions/initialStateFromFirebase";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const enhancer = composeEnhancers(
-  persistState()
-);
+const enhancer = composeEnhancers(persistState(), applyMiddleware(thunk));
 
-const store = createStore(dinnersApp, initialState, enhancer);
+const store = createStore(dinnersApp, enhancer);
+
+store.dispatch(getInitialState());
 
 ReactDOM.render(
   <Provider store={store}>
